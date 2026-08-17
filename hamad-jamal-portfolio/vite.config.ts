12e49c7d88c5cb1a421a79072576f150510e6fb7
popbean <pipben7@gmail.com>
2026-08-18 00:48:34 +0300

    Delete hamad-jamal-portfolio directory

--- hamad-jamal-portfolio/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { sites } from "@openai/sites-vite-plugin";
-
-export default defineConfig({
-  base: "./",
-  plugins: [react(), sites()],
-});
